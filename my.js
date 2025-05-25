030ebecf7bb0c98ef4c167a74924311fce06299a
tumanchik21 <i@mbondarchik.ru>
2025-05-25 16:31:16 +0300

    App.js

--- my.js
+++ my.js
@@ -1,1 +1,21 @@
-db
+import React, { useState } from "react";
+import ProductList from "./ProductList";
+import Cart from "./Cart";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <div>
+      <h1>Магазин</h1>
+      <ProductList addToCart={addToCart} />
+      <Cart cart={cart} />
+    </div>
+  );
+}
+
+export default App;

949535cd00828133395fa7eaef44339578120ed3
tumanchik21 <i@mbondarchik.ru>
2025-05-25 16:36:06 +0300

    Sec/ProductList.js

---
+++ ProductList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const products = [
+  { id: 1, name: "Товар 1", price: 100 },
+  { id: 2, name: "Товар 2", price: 200 },
+];
+
+function ProductList({ addToCart }) {
+  return (
+    <div>
+      <h2>Каталог товаров</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <p>{product.name} - {product.price}₽</p>
+          <button onClick={() => addToCart(product)}>Добавить в корзину</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ProductList;
